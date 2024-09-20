216c0989f7332d5428187647c7a6b8806d53986f
root <root@DELL-7559>
2024-09-20 18:58:50 -0300

    Projeto melhorado, dinâmico

--- src/main/java/Exercicio27.java
+++ src/main/java/Exercicio27.java
@@ -1,7 +1,49 @@
+// IMPORTS
+
+import java.util.Random;
+import java.util.Scanner;
 
 public class Exercicio27 {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+	public static void main(String[] args) {
+		// INSTANTIATE
+		Scanner sc = new Scanner(System.in);
+		Random rand = new Random();
+
+		// DECLARATE
+		int size = 20;
+		int limit = 50;
+		int db[] = new int[size];
+		int search;
+		boolean ok = false;
+
+		for (int i = 0; i < size; i++) {
+			db[i] = rand.nextInt(limit);
+		}
+
+		// COLLECT DATA
+		System.out.print("Number to search : ");
+		search = sc.nextInt();
+
+		// OUTPUT
+		System.out.println("++++++++++++++++++++++++++++++++++++++++++++");
+		System.out.println("+ DATABASE");
+		System.out.println("++++++++++++++++++++++++++++++++++++++++++++");
+		for (int i = 0; i < size; i++) {
+			System.out.print(db[i] + " ");
+		}
+		System.out.println("\n\n");
+
+		for (int i = 0; i < size; i++) {
+			if (db[i] == search) {
+				System.out.println(db[i] + " found at index " + i);
+				ok = true;
+				break;
+			}
+		}
+
+		if (ok == false) {
+			System.out.println(search + " not found");
+		}
+	}
 }
